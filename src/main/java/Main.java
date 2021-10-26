a4eedc5c510c23cb6bdfa1ce7496611d4fa8ed2a
Muvi7z <ochir-zhamsuev@mail.ru>
2021-10-26 12:15:41 +0800

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,27 @@ import io.jenetics.util.Factory;
 
 import java.util.Random;
 
-public class Main {
+import static java.lang.Long.toUnsignedString;
 
-     public class GeneticEngene {
+public class Main {
+     public static String toBinaryString(int i) {
+          return toUnsignedString(i, 1);
+     }
+     public static class GeneticEngene {
           String[] phenotype ={};
           String[] population = {};
           GeneticEngene(){
-               Random random = new Random();
-               
+               for(int i=0; i<5; i++){
+                    Random random = new Random();
+                    int a = random.nextInt(15);
+                    phenotype[i]= Integer.toString(a, 2);
+               }
+
+               System.out.println();
           }
      }
 
      public static void main(String[] args) {
+          GeneticEngene f = new GeneticEngene();
      }
 }
