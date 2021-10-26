76b920099800b80cd5009d76e2a414bc64573527
Muvi7z <ochir-zhamsuev@mail.ru>
2021-10-26 12:01:26 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+
+import io.jenetics.*;
+import io.jenetics.engine.Engine;
+import io.jenetics.engine.EvolutionResult;
+import io.jenetics.util.Factory;
+
+import java.util.Random;
+
+public class Main {
+
+     public class GeneticEngene {
+          String[] phenotype ={};
+          String[] population = {};
+          GeneticEngene(){
+               Random random = new Random();
+               
+          }
+     }
+
+     public static void main(String[] args) {
+     }
+}
