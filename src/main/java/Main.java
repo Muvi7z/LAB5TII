edfa10a26992f3d1f7f80d80badabaf41a9facf6
Muvi7z <ochir-zhamsuev@mail.ru>
2021-11-17 20:19:39 +0800

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,31 +25,44 @@ public class Main {
           public Double getFitness() {
                return fitness;
           }
-
+          public static void printPopulations(ArrayList<Population> populations){
+               populations.stream().map(x->x.sign).forEach(System.out::println);
+          }
           public int getSumBit() {
                return sumBit;
           }
      }
      public static class GeneticEngine {
-          String[] phenotype =new String[10];
+          ArrayList<String> phenotype =new ArrayList<>();
+          ArrayList<String> phenotypeCodeGrey =new ArrayList<>();
+          ArrayList<Population> mutationPopulations = new ArrayList<>();
+          Population maxPopulation = new Population("");
           String[] codeGray ={"0000","0001","0011","0010","0110","0111","0101","0100","1100","1101","1111","1110","1010","1011","1001","1000"};
           ArrayList<Population> populations = new ArrayList<>();
           ArrayList<Population> selectedPopulations = new ArrayList<>();
+          ArrayList<Population> newPopulations = new ArrayList<>();
           GeneticEngine(){
                Random random = new Random();
-               for(int i=0; i<5; i++){
+               System.out.println("Признаки:");
+               while (phenotype.size() < 5) {
                     int a = random.nextInt(14)+1;
-                    phenotype[i]= Integer.toString(a, 2);
-                    phenotype[i]= "0000".substring(phenotype[i].length())+phenotype[i];
-                    phenotype[i+5]=codeGray[a];
+                    String phen = Integer.toString(a, 2);
+                    phen="0000".substring(phen.length())+phen;
+                    if (!phenotype.contains(phen)) {
+                         phenotype.add(phen);
+                         phenotypeCodeGrey.add(codeGray[a]);
+                         System.out.println(a+" - "+ phen +" - "+ codeGray[a]);
+
+                    }
                }
+               phenotype.addAll(phenotypeCodeGrey);
+
           }
           public void generatePopulation(){
+               System.out.println("Сформированная популяция:");
                for(int i=0; i<5; i++){
-                    populations.add(new Population(phenotype[0]+phenotype[i+2]));
-                    populations.add(new Population(phenotype[1]+phenotype[i+2]));
-//                    System.out.println(population[i*2]);
-//                    System.out.println(population[(i*2)+1]);
+                    populations.add(new Population(phenotype.get(0)+phenotype.get(i+2)));
+                    populations.add(new Population(phenotype.get(1)+phenotype.get(i+2)));
                }
           }
 
@@ -59,37 +72,89 @@ public class Main {
 
           public void fitnessFun(){
                int min =8;
+               int max =0;
                for (Population individ: populations) {
                     int sum = individ.sign.replaceAll("0","").length();
                     individ.setSumBit(sum);
                     if(sum<min){
                          min = sum;
                     }
+                    if(sum>max){
+                         max = sum;
+                         maxPopulation = individ;
+                    }
                }
+               System.out.println("Фитнес функция:");
                for (Population individ: populations) {
                     individ.fitness = min * individ.sumBit;
+                    System.out.println(individ.sign+" - Fitness: "+individ.fitness +" - Sum: " + individ.sumBit);
                }
           }
           public void tournamentSelection(){
-               for (int i=0; i<populations.size(); i++){
+               System.out.println("Особи прошедшие отбор:");
+               for (int i=0; i<8; i++){
                     Population selected = new Population("0");
                     Random random = new Random();
-                    selected = populations.get(random.ints(3,0,populations.size()).boxed().reduce((x,y) -> {
+                    selected = populations.get(random.ints(2,0,populations.size()).boxed().reduce((x,y) -> {
                          if(populations.get(x).fitness<populations.get(y).fitness){
                               return y;
                          }
                          return x;
                     }).get());
                     selectedPopulations.add(selected);
+                    int j = selectedPopulations.indexOf(selected);
+                    System.out.println(j +1+") "+ selected.sign);
                }
+          }
+          public void twoPointCrossover(){
+               Random random = new Random();
+               int fPoint = random.nextInt(4)+2;
+               int sPoint = random.nextInt(4-(fPoint-2))+4+(fPoint-2);
+               System.out.println("Популяция после скрещивания");
+               for(int i=0; i<selectedPopulations.size()/2; i++){
+                    String pathF= selectedPopulations.get(i*2).sign.substring(0,fPoint)+selectedPopulations.get((i*2)+1).sign.substring(fPoint, sPoint)+selectedPopulations.get(i*2).sign.substring(sPoint);
+                    selectedPopulations.get(i*2).sign = selectedPopulations.get((i*2)+1).sign.substring(0,fPoint)+
+                            selectedPopulations.get((i*2)).sign.substring(fPoint,sPoint)+
+                            selectedPopulations.get((i*2)+1).sign.substring(sPoint);
+                    selectedPopulations.get((i*2)+1).sign=pathF;
+                    System.out.println(selectedPopulations.get(i*2).sign);
+                    System.out.println(pathF);
+               }
+          }
+          public void translocationMutation(){
+
+               for(Population ind: selectedPopulations){
+                    Random random = new Random();
+                    int percent =random.nextInt(101);
+                    if(percent<=30){
+                         mutationPopulations.add(new Population(ind.sign.substring(0,2)+ind.sign.substring(5,7)+ind.sign.substring(2,5)+
+                                 ind.sign.substring(7)));
+                         //ind.sign = temp;
+                    }
+               }
+          }
+          public void generateNewPop(){
+               System.out.println("Популяция наследников:");
+               Population.printPopulations(selectedPopulations);
+               newPopulations.addAll(selectedPopulations);
+               System.out.println("Мутировавшие особи:");
+               Population.printPopulations(mutationPopulations);
+               newPopulations.addAll(mutationPopulations);
+               System.out.println("Особь по принципу Элитизма:");
+               System.out.println(maxPopulation.sign);
           }
      }
 
      public static void main(String[] args) {
           GeneticEngine f = new GeneticEngine();
           f.generatePopulation();
+          Population.printPopulations(f.populations);
           f.fitnessFun();
           f.tournamentSelection();
+
+          f.twoPointCrossover();
+          f.translocationMutation();
+          f.generateNewPop();
           f.getPopulations();
      }
 }
